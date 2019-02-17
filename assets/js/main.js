31e7e3bc27d899dc543ed201bece16605e34ce55
Kingaa6 <Kingaa6@gmail.com>
2019-02-17 13:13:50 -0800

    Add form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,13 @@
     });
   }
 })(jQuery);
+
+let email = document.getElementById('email');
+
+email.addEventListener('input', function(event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('Please enter a valid email.');
+  } else {
+    email.setCustomValidity('');
+  }
+});
